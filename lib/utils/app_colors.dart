2de7430ba5b56adf3566ca6ed3cec61d7971b519
youssef <youssefezzat937@gmail.com>
2026-02-08 17:32:39 +0200

    + initialize project. + added empty home screen. + added colors and routes names

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFE2BE7A);
+  static const Color blackColor = Color(0xFF202020);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color transparentColor = Colors.transparent;
+  static const Color blackBgColor = Color(0x99202020);
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,3 @@
+class AppRoutes {
+  static const homeRouteName = 'home_screen';
+}
